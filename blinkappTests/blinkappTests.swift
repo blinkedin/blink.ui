bff5f1c70ff0f1103241fafb992dfeb89034a360
pilgwon <rlavlfrnjs12@gmail.com>
2017-10-30 21:47:30 +0900

    Initial Commit

---
+++ blinkappTests/blinkappTests.swift
@@ -0,0 +1,36 @@
+//
+//  blinkappTests.swift
+//  blinkappTests
+//
+//  Created by PilGwonKim on 2017. 10. 30..
+//  Copyright © 2017년 PilGwonKim. All rights reserved.
+//
+
+import XCTest
+@testable import blinkapp
+
+class blinkappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
